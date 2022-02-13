4e2e55e2580ef795b296b002580473bbc0a0c6f4
darshan4416 <darshanhirekurubar4416@gmail.com>
2022-02-14 00:21:45 +0530

    upload_file commited

---
+++ app.py
@@ -0,0 +1,26 @@
+import boto3
+import requests
+from botocore.exceptions import NoCredentialsError
+
+
+ACCESS_KEY = ''
+SECRET_KEY = ''
+
+def upload_to_bucket(local_file, bucket, s3_file):
+        s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                      aws_secret_access_key=SECRET_KEY)
+
+        try:
+            s3.upload_file(local_file, bucket, s3_file)
+            print("Upload Successful")
+            return True
+        except FileNotFoundError:
+            print("The file was not found")
+            return False
+        except NoCredentialsError:
+            print("Credentials not available")
+            return False
+
+uploaded = upload_to_bucket('data.csv','pythonbucket2022','data_2.csv')
+
+
